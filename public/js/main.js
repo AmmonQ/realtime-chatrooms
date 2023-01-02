06188118a8078b35089c89d49e8ce5285573b143
AmmonQ <aq229@byu.edu>
2023-01-02 12:21:59 -0700

    Initial commit

---
+++ public/js/main.js
@@ -0,0 +1,89 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+const chatInput = document.getElementById('msg');
+const typingMessage = document.getElementById('typing-message');
+
+// get username and room from URL
+const {username, room} = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+});
+
+const socket = io();
+
+// join chatroom
+socket.emit('joinRoom', { username, room });
+
+// get room and users
+socket.on('roomUsers', ({room, users}) => {
+    outputRoomName(room);
+    outputUsers(users);
+})
+
+// message from server
+socket.on('message', message => {
+    outputMessage(message);
+    console.log(message);
+
+    // scroll down every time a message is recieved from the server
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+socket.on('typing message', message => {
+    outputTypingMessage(message);
+
+    window.setTimeout(clearTypingMessage, 1000);
+})
+
+// message submit
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const msg = e.target.elements.msg.value;
+
+    // emit message to server
+    socket.emit('chatMessage', msg);
+
+    // clear input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+});
+
+chatInput.addEventListener('keyup', (e) => {
+    if (e.code !== 'Enter') {
+        socket.emit('typing', username);
+    }
+});
+
+// output message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `
+						<p class="meta">${message.username} <span>${message.time}</span></p>
+						<p class="text">
+							${message.text}
+						</p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+}
+
+// add room name to DOM
+function outputRoomName(room) {
+    roomName.innerText = room;
+}
+
+// add users to DOM
+function outputUsers(users) {
+    userList.innerHTML = `
+        ${users.map(user => `<li>${user.username}</li>`).join('')}
+    `;
+}
+
+function outputTypingMessage(message) {
+    typingMessage.innerText = message;
+}
+
+function clearTypingMessage() {
+    typingMessage.innerText = '';
+}
